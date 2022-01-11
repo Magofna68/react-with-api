f00dbd3ece4a688daa7c364e4d804c4447c5f64a
Brandon-Magofna <magofna68@gmail.com>
2022-01-10 16:32:45 -0800

    update file structure to include components: headlines & app.js.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    {/* The Headlines component will have the bulk of our code. 
+    Because we will be using state, it will be a class component. */}
+    <Headlines/>  
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Headlines.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+class Headlines extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            //  The initial state of isLoaded is false. 
+            // Our component will mount before we make an API call. 
+            // We'll put the code for the API call in the componentDidMount
+            //  lifecycle method so it runs as soon as our component is mounted. 
+            // Once the call is complete, isLoaded will be switched to true
+            headlines: []
+            // headlines is an empty array. 
+            // It will contain the NYT headlines in JSON format.
+
+        };
+    }
+
+    makeApiCall = () => {
+        
+            // We use the built-in fetch() method (JavaScript, not React) to make our API call. 
+            // The URL for the API call is in backticks so that we can use a template string for 
+            // our API key, which is stored in an environmental variable.
+
+            fetch(`https:/api.nytimes.com/svc/topstories/v2/home.json?api-key=$process.env.REACT_APP_API_KEY}`)
+            
+            // Once the API call is complete, the response will be converted to JSON. 
+            // Then, once it's been converted, we'll have our results.
+            
+            .then(response => response.json())
+            .then(
+                (jsonifiedResponse) => {
+                    this.setState({
+                        isLoaded: true,
+                        headlines: jsonifiedResponse.results
+                        
+                        // the headlines are stored in a property of the response called results, 
+                        // we update the headlines state to be jsonifiedResponse.results
+                        
+                    });
+                })
+                .catch((error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                        // Otherwise, we set isLoaded to true and set the error property
+                        //  to the error that the API call returns
+                    });
+                });
+            }
+            
+            compoenentDidMount() {
+                this.makeApiCall()
+            }
+            
+            render() {
+                const {error, isLoaded, headlines } = this.state;
+                if(error) {
+                    // return <React.Fragment>Loading...</React.Fragment>;
+                    return <>Error: {error.message}</>;
+                } else if (!isLoaded) {
+                    return <>Loading...</>;
+                } else {
+                    return (
+                        <>
+                    <h1> Headlines </h1>
+                    <ul>
+                        {headlines.map((headline, index) =>
+                            <li key={index}>
+                                <h3>{headline.title}</h3>
+                                <p>{headline.abstract}</p>
+                            </li>
+                        )}
+                    </ul>
+                    </>
+                );
+            }
+        }
+        
+};
+
+
+export default Headlines;
