50eed91fc39dcce0a3cadee338b9c766fd62c8db
Brandon-Magofna <magofna68@gmail.com>
2022-01-11 15:12:15 -0800

    add container elemeent to app

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Headlines from './components/Headlines'
 function App() {
   return (
     <React.Fragment>
+      <Background>
     {/* The Headlines component will have the bulk of our code. 
     Because we will be using state, it will be a class component. */}
-    <Headlines/>  
+          <Headlines/>
+      </Background>
     </React.Fragment>
   );
 }
